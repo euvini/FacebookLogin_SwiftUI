4d17c769c62a2a7353db88ae448aa32897d273f5
Vinicius Santiago <vinicius.eduardo@kognit.com.br>
2023-07-27 14:35:11 -0300

    Initial Commit

---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Vinicius Santiago on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
