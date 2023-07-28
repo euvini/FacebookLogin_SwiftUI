fcd3e59ed49787c3a8a55a0b9e04e98751291037
Vinicius Santiago <vinicius.eduardo@kognit.com.br>
2023-07-28 14:19:45 -0300

    Initial commit

--- Facebook/ContentView.swift
+++ Facebook/ContentView.swift
@@ -8,15 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer().frame(height: 64)
+                
+                VStack {
+                    TextField("Email", text: $email)
+                        .padding(.top, 24)
+                        .padding(.bottom, 12)
+                        .padding(.horizontal)
+                        .keyboardType(.emailAddress)
+                    
+                    Divider()
+                    
+                    SecureField("Password", text: $password)
+                        .padding(.top, 12)
+                        .padding(.bottom, 24)
+                        .padding(.horizontal)
+                }.background(Color.white)
+                    .cornerRadius(4.0)
+                
+                Spacer().frame(height: 16)
+                
+                Button(action: {}) {
+                    Text("Login")
+                        .font(.title3)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(20)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("ButtonColor"))
+                        .cornerRadius(4.0)
+                }
+                
+                Spacer().frame(height: 64)
+                
+                VStack(spacing: 8) {
+                    Button(action: {}) {
+                        Text("Don't have an account? Sign up.")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    
+                    Button(action: {}) {
+                        Text("Forgot your password?")
+                            .foregroundColor(Color("TextColor"))
+                    }
+                }
+                
+                
+            }.padding()
+            
         }
-        .padding()
     }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
